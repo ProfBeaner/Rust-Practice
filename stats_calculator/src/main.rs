87bc0cfffbf09cfaee85ada856d7eac2adf61db7
SMS <smsb13015@gmail.com>
2025-11-12 15:23:14 +0000

    Switched dependencies from Cargo.toml to Cargo2.toml for stats_calculator crate.

--- stats_calculator/src/main.rs
+++ stats_calculator/src/main.rs
@@ -1,3 +1,4 @@
-fn main() {
-    println!("Hello, world!");
-}
+//11/11/2025: This rust program calculates rudimentary statistics such as: 
+//measures of central tendency (mean, median, mode)
+//measures of variability or dispersion (range, variance, standard deviation)
+//measures of relative standing (quartiles, percentiles, z-scores)
