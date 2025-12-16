578615aa599533f97886e96ab30365e7f4b04cf0
SMS <smsb13015@gmail.com>
2025-12-16 18:30:50 +0000

    Deleted stats calculator project due to lack of interest

--- stats_calculator/src/main.rs
+++
@@ -1,4 +0,0 @@
-//11/11/2025: This rust program calculates rudimentary statistics such as: 
-//measures of central tendency (mean, median, mode)
-//measures of variability or dispersion (range, variance, standard deviation)
-//measures of relative standing (quartiles, percentiles, z-scores)
